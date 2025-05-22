f2ce5a67c565b61be8cb1c944fc856219226c35e
zizaial007 <reziaurrehman@gmail.com>
2025-05-22 14:17:24 +0500

    change

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import CardSlice from './CardSlice'
 
 
- const store=configureStore({
+ const Store=configureStore({
     reducer:{
         cart:CardSlice
     }
 })
-export default store
+export default Store
